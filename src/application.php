04a3324fdc5b7b358095f1df9043fe8ee26fda5c
Sanpi <sanpi@homecomputing.fr>
2014-12-15 22:14:01 +0100

    Fix indentation

--- src/application.php
+++ src/application.php
@@ -52,7 +52,7 @@ function generateIndex($root, $path, $level = 0)
         return $summary;
     }
 
-    $indent = str_pad(' ', $level * 4);
+    $indent = str_pad('', $level * 4);
     foreach (new \Sanpi\SortableDirectoryIterator($path) as $fileInfo) {
         $filename = $fileInfo->getFilename();
         $title = str_replace('.md', '', $filename);
